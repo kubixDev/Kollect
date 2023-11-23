32605edf0861d7962846f731e5de3efc056c6ee8
Kuba <62606910+kubixDev@users.noreply.github.com>
2023-11-23 03:07:00 +0100

    updated search fragment layout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,4 +40,6 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("com.intuit.sdp:sdp-android:1.1.0")
+    implementation("androidx.cardview:cardview:1.0.0")
+    implementation("androidx.recyclerview:recyclerview:1.0.0")
 }
