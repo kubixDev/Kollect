511c6afe9774caf8ea65297c5e743daea260508d
Kuba <62606910+kubixDev@users.noreply.github.com>
2023-11-26 21:56:52 +0100

    added radio buttons to collection fragment

--- app/src/main/java/com/kubixdev/kollect/ThirdFragment.java
+++ app/src/main/java/com/kubixdev/kollect/ThirdFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioButton;
 
 import androidx.fragment.app.Fragment;
 
@@ -15,6 +16,12 @@ public class ThirdFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_collection, container, false);
+        View view = inflater.inflate(R.layout.fragment_collection, container, false);
+
+        // default selection for radio button group
+        RadioButton ownedButton = view.findViewById(R.id.ownedButton);
+        ownedButton.setChecked(true);
+
+        return view;
     }
 }
