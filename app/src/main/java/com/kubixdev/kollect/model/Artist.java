7470f696737a7ba12bb1f4f1a4a9baf2d6e7bd68
Kuba <62606910+kubixDev@users.noreply.github.com>
2024-01-06 03:30:24 +0100

    added artist and member model

---
+++ app/src/main/java/com/kubixdev/kollect/model/Artist.java
@@ -0,0 +1,82 @@
+package com.kubixdev.kollect.model;
+
+import android.util.Log;
+
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+
+public class Artist {
+    private ArrayList<String> memberIds;
+    private String image;
+
+    // default constructor
+    public Artist() {
+    }
+
+    // constructor with parameters
+    public Artist(ArrayList<String> memberIds, String image) {
+        this.memberIds = memberIds;
+        this.image = image;
+    }
+
+    public ArrayList<String> getMemberIds() {
+        return memberIds;
+    }
+
+    public void setMemberIds(ArrayList<String> memberIds) {
+        this.memberIds = memberIds;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    // static method to retrieve artist data from firestore for any artist
+    public static void loadArtistData(String artistName, final Artist.ArtistDataLoadListener listener) {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        db.collection("artists")
+                .document(artistName)
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        DocumentSnapshot document = task.getResult();
+
+                        if (document.exists()) {
+                            Artist artist = document.toObject(Artist.class);
+
+                            if (artist != null) {
+                                listener.onArtistDataLoaded(artist);
+                            }
+                            else {
+                                listener.onArtistDataLoadFailed("Failed to convert document to Artist object");
+                                Log.e("ARTISTINFO", "Failed to convert document to Artist object");
+                            }
+
+                        }
+                        else {
+                            listener.onArtistDataLoadFailed("No such document");
+                            Log.e("ARTISTINFO", "No such document");
+                        }
+
+                    }
+                    else {
+                        listener.onArtistDataLoadFailed("Task failed with exception: " + task.getException());
+                        Log.e("ARTISTINFO", "Task failed with exception: " + task.getException());
+                    }
+                });
+    }
+
+
+    // interface for callbacks when artist data is loaded
+    public interface ArtistDataLoadListener {
+        void onArtistDataLoaded(Artist artist);
+        void onArtistDataLoadFailed(String error);
+    }
+}
---
+++ app/src/main/java/com/kubixdev/kollect/model/Member.java
@@ -0,0 +1,112 @@
+package com.kubixdev.kollect.model;
+
+import android.util.Log;
+
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+
+public class Member {
+    private String firstName;
+    private String lastName;
+    private String artistId;
+    private ArrayList<String> photocardIds;
+    private String image;
+
+    // default constructor
+    public Member() {
+    }
+
+    // constructor with parameters
+    public Member(String firstName, String lastName, String artistId, ArrayList<String> photocardIds, String image) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.artistId = artistId;
+        this.photocardIds = photocardIds;
+        this.image = image;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getArtistId() {
+        return artistId;
+    }
+
+    public void setArtistId(String artistId) {
+        this.artistId = artistId;
+    }
+
+    public ArrayList<String> getPhotocardIds() {
+        return photocardIds;
+    }
+
+    public void setPhotocardIds(ArrayList<String> photocardIds) {
+        this.photocardIds = photocardIds;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    // static method to retrieve member data from firestore for any member
+    public static void loadMemberData(String memberName, final Member.MemberDataLoadListener listener) {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        db.collection("members")
+                .document(memberName)
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        DocumentSnapshot document = task.getResult();
+
+                        if (document.exists()) {
+                            Member member = document.toObject(Member.class);
+
+                            if (member != null) {
+                                listener.onMemberDataLoaded(member);
+                            }
+                            else {
+                                listener.onMemberDataLoadFailed("Failed to convert document to Member object");
+                                Log.e("MEMBERINFO", "Failed to convert document to Member object");
+                            }
+
+                        }
+                        else {
+                            listener.onMemberDataLoadFailed("No such document");
+                            Log.e("MEMBERINFO", "No such document");
+                        }
+
+                    }
+                    else {
+                        listener.onMemberDataLoadFailed("Task failed with exception: " + task.getException());
+                        Log.e("MEMBERINFO", "Task failed with exception: " + task.getException());
+                    }
+                });
+    }
+
+
+    // interface for callbacks when member data is loaded
+    public interface MemberDataLoadListener {
+        void onMemberDataLoaded(Member member);
+        void onMemberDataLoadFailed(String error);
+    }
+}
